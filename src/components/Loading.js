1dfcccd7d5619b04e4187fefe2420d0c2fbd77ad
nicogulo <nicogulobelas@gmail.com>
2022-02-04 20:41:06 +0700

    feat: call for paper mobile

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,9 +1,9 @@
-import React from 'react';
+import React from "react";
 
 function Loading() {
   return (
-    <div className='loading-container'>
-      <div className='loading'></div>;
+    <div className="loading-container">
+      <div className="loading"></div>;
     </div>
   );
 }
--- src/pages/Call-for-paper/index.js
+++ src/pages/Call-for-paper/index.js
@@ -1,11 +1,11 @@
 /* eslint-disable jsx-a11y/iframe-has-title */
 import "./Callforpaper.css";
-import Loading from "../../components/Loading";
 import { useQuery } from "@apollo/client";
 import { LOAD_CALL_FOR_PAPER } from "../../graphql/queries";
+// import { Link } from "react-scroll";
+import Loading from "../../components/Loading";
 import ImportantDates from "../../components/ImportantDates";
-// import { useState } from 'react';
-import { Link } from "react-scroll";
+import Scope from "../../components/Scope";
 import Map from "../../components/Map";
 const locations = require("./location.json");
 
@@ -38,172 +38,20 @@ const CallForPaper = () => {
         </div>
       </div>
       {/* ENDING HERO */}
+      {/* IMPORTAN DATES */}
       <ImportantDates />
-
+      {/* END IMPORTANT DATES */}
+      <section className="main">
+        <p>{datas.penjelasan}</p>
+        <div className="text">Topic Interest submission limited into :</div>
+      </section>
+      {/* SCOPE */}
+      <Scope />
+      {/* END SCOPE */}
       <div className="call-for-paper-container">
         <div className="call-for-paper-flex">
-          <div className="public">
-            {/* MAIN */}
-            <section className="main">
-              <p>{datas.penjelasan}</p>
-              <div className="text">
-                Topic Interest submission limited into :
-              </div>
-            </section>
-
-            {/* END MAIN */}
-            {/* PUBLICATION */}
-            <section className="publications" id="frontend">
-              <h2>{datas.judul1}</h2>
-              <p>{datas.deskripsi1}</p>
-              {/* adding wrap */}
-              <div className="flex gap-100 ">
-                <div className="col gap-16">
-                  <div className="links">
-                    <img src="/assets/check.svg" alt="check" /> {datas.energy1}
-                  </div>
-                  <div className="links">
-                    <img src="/assets/check.svg" alt="check" /> {datas.energy2}
-                  </div>
-                  <div className="links">
-                    <img src="/assets/check.svg" alt="check" /> {datas.energy3}
-                  </div>
-                  <div className="links">
-                    <img src="/assets/check.svg" alt="check" /> {datas.energy4}
-                  </div>
-                </div>
-
-                <div className="col gap-16">
-                  <div className="links">
-                    <img src="/assets/check.svg" alt="check" /> {datas.energy5}
-                  </div>
-                  <div className="links">
-                    <img src="/assets/check.svg" alt="check" /> {datas.energy6}
-                  </div>
-                  <div className="links">
-                    <img src="/assets/check.svg" alt="check" /> {datas.energy7}
-                  </div>
-                  <div className="links">
-                    <img src="/assets/check.svg" alt="check" /> {datas.energy8}
-                  </div>
-                </div>
-              </div>
-            </section>
-
-            <section className="publications" id="security">
-              <h2>{datas.judul2}</h2>
-              <p>{datas.deskripsi2}</p>
-              <div className="flex  gap-100">
-                <div className="col gap-16">
-                  <div className="links">
-                    <img src="/assets/check.svg" alt="check" /> {datas.it1}
-                  </div>
-                  <div className="links">
-                    <img src="/assets/check.svg" alt="check" /> {datas.it2}
-                  </div>
-                  <div className="links">
-                    <img src="/assets/check.svg" alt="check" /> {datas.it3}
-                  </div>
-                  <div className="links">
-                    <img src="/assets/check.svg" alt="check" /> {datas.it4}
-                  </div>
-                </div>
-
-                <div className="col gap-16">
-                  <div className="links">
-                    <img src="/assets/check.svg" alt="check" /> {datas.it5}
-                  </div>
-                  <div className="links">
-                    <img src="/assets/check.svg" alt="check" /> {datas.it6}
-                  </div>
-                  <div className="links">
-                    <img src="/assets/check.svg" alt="check" /> {datas.it7}
-                  </div>
-                  <div className="links">
-                    <img src="/assets/check.svg" alt="check" /> {datas.it8}
-                  </div>
-                </div>
-              </div>
-            </section>
-
-            <section className="publications" id="ai">
-              <h2>{datas.judul3}</h2>
-              <p>{datas.deskripsi3}</p>
-              <div className="flex gap-100 ">
-                <div className="col gap-16">
-                  <div className="links">
-                    <img src="/assets/check.svg" alt="check" /> {datas.ai1}
-                  </div>
-                  <div className="links">
-                    <img src="/assets/check.svg" alt="check" /> {datas.ai2}
-                  </div>
-                  <div className="links">
-                    <img src="/assets/check.svg" alt="check" /> {datas.ai3}
-                  </div>
-                  <div className="links">
-                    <img src="/assets/check.svg" alt="check" /> {datas.ai4}
-                  </div>
-                </div>
-
-                <div className="col gap-16">
-                  <div className="links">
-                    <img src="/assets/check.svg" alt="check" /> {datas.ai5}
-                  </div>
-                  <div className="links">
-                    <img src="/assets/check.svg" alt="check" /> {datas.ai6}
-                  </div>
-                  <div className="links">
-                    <img src="/assets/check.svg" alt="check" /> {datas.ai7}
-                  </div>
-                  <div className="links">
-                    <img src="/assets/check.svg" alt="check" /> {datas.ai8}
-                  </div>
-                </div>
-              </div>
-            </section>
-
-            <section className="publications" id="iot">
-              <h2>{datas.judul4}</h2>
-              <p>{datas.deskripsi4}</p>
-              {/* adding wrap */}
-              {/* adding justify content = space between */}
-              <div className="flex gap-100 ">
-                <div className="col gap-16">
-                  <div className="links">
-                    <img src="/assets/check.svg" alt="check" /> {datas.iot1}
-                  </div>
-                  <div className="links">
-                    <img src="/assets/check.svg" alt="check" /> {datas.iot2}
-                  </div>
-                  <div className="links">
-                    <img src="/assets/check.svg" alt="check" /> {datas.iot3}
-                  </div>
-                  <div className="links">
-                    <img src="/assets/check.svg" alt="check" /> {datas.iot4}
-                  </div>
-                </div>
-
-                <div className="col gap-16">
-                  <div className="links">
-                    <img src="/assets/check.svg" alt="check" /> {datas.iot5}
-                  </div>
-                  <div className="links">
-                    <img src="/assets/check.svg" alt="check" /> {datas.iot6}
-                  </div>
-                  <div className="links">
-                    <img src="/assets/check.svg" alt="check" /> {datas.iot7}
-                  </div>
-                  <div className="links">
-                    <img src="/assets/check.svg" alt="check" /> {datas.iot8}
-                  </div>
-                </div>
-              </div>
-            </section>
-            {/* END PUBLICATION */}
-          </div>
-
           {/* SIDEBAR */}
-          <aside>
+          {/* <aside>
             <div className="menu-card">
               <p className="sidebar-title">fields</p>
               <nav>
@@ -264,11 +112,10 @@ const CallForPaper = () => {
                 </ul>
               </nav>
             </div>
-          </aside>
+          </aside> */}
           {/* END SIDEBAR */}
         </div>
       </div>
-
       <div className="cardContainer">
         <div className="card">
           <div className="cardContent">
